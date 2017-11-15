e047019230a65158ec1eb4afd2514be2c638944c
pferracini <pedro.ferracini@sjrp.unesp.br>
2017-11-15 00:36:33 -0200

    Inicio analisador lexico-sintatico

---
+++ trab2/sintatico.tab.h
@@ -0,0 +1,103 @@
+/* A Bison parser, made by GNU Bison 3.0.4.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_SINTATICO_TAB_H_INCLUDED
+# define YY_YY_SINTATICO_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    INTEIRO = 258,
+    REAL = 259,
+    ABREPAR = 260,
+    FECHAPAR = 261,
+    CHAR = 262,
+    FIMLINHA = 263,
+    ADICAO = 264,
+    SUBTRACAO = 265,
+    MULTIPLICACAO = 266,
+    DIVISAO = 267,
+    MAIOR = 268,
+    MENOR = 269,
+    MAIORIGUAL = 270,
+    MENORIGUAL = 271,
+    DIFERENTE = 272,
+    NEGACAO = 273,
+    IGUAL = 274,
+    TIPOVAR = 275,
+    IFCONDICAO = 276,
+    ELSECONDICAO = 277,
+    FORREPETICAO = 278,
+    WHILEREPETICAO = 279,
+    FICONDICAO = 280,
+    VARIAVEL = 281,
+    METODO = 282,
+    ATRIBUICAO = 283,
+    STRING = 284
+  };
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+
+union YYSTYPE
+{
+#line 10 "sintatico.y" /* yacc.c:1909  */
+
+    int inteiro;
+    float real;
+    char simbolo;
+    char *string_simbolo;
+
+#line 91 "sintatico.tab.h" /* yacc.c:1909  */
+};
+
+typedef union YYSTYPE YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_SINTATICO_TAB_H_INCLUDED  */
---
+++ trab2/sintatico.y
@@ -0,0 +1,35 @@
+/*
+Esse é um arquivo feito para o uso no BISON, sendo usado para o trabalho de compiladores.
+*/
+
+%{
+#include <stdio.h>
+%}
+
+/*  Valor semantico dos simbolos    */
+%union {
+    int inteiro;
+    float real;
+    char simbolo;
+    char *string_simbolo;
+};
+
+/*  Tokens  */
+%token <inteiro> INTEIRO
+%token <real> REAL
+%token <simbolo> ABREPAR FECHAPAR CHAR FIMLINHA ADICAO SUBTRACAO MULTIPLICACAO DIVISAO MAIOR MENOR MAIORIGUAL MENORIGUAL DIFERENTE NEGACAO IGUAL
+%token <string_simbolo> TIPOVAR IFCONDICAO ELSECONDICAO FORREPETICAO WHILEREPETICAO FICONDICAO VARIAVEL METODO ATRIBUICAO STRING
+
+/*  Regras da Gramatica */
+%start tipoReturn
+%%
+
+tipoReturn:
+    TIPOVAR METODO {printf ("TipoVar: %s\nNomeMetodo: %s\n", $1, $2);}
+    ;
+%%
+
+int main (void){
+    yyparse();
+    return 0;
+}
